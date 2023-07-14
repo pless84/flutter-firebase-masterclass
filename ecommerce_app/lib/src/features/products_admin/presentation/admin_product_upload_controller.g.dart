e3bd8e3bebf97fedf61d5ebf084b5d6ab899f4f4
Andrea Bizzotto <codewithandrea@gmail.com>
2023-07-14 08:58:05 +0100

    Starter project for section 7: Convert `cartTotalProvider` to a FutureProvider<double>

--- ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
+++ ecommerce_app/lib/src/features/products_admin/presentation/admin_product_upload_controller.g.dart
@@ -7,7 +7,7 @@ part of 'admin_product_upload_controller.dart';
 // **************************************************************************
 
 String _$adminProductUploadControllerHash() =>
-    r'dc688fd3753b06f978d3bfe30feaa4edf57e32d1';
+    r'15e2c67290aa554d75c956b1a81aabb35484cc24';
 
 /// See also [AdminProductUploadController].
 @ProviderFor(AdminProductUploadController)
